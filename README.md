04fab3ef4ae1697eafe4d968053af108ea10b745
Henrique Nepomuceno <nepo26.hn@gmail.com>
2020-06-29 12:54:16 -0300

    Basic structures made and few functions to understand moves

--- README.md
+++ README.md
@@ -11,3 +11,16 @@ To start we need to determine the basics, which will be how this API will work. 
 - D - Diamonds
 - H - Hearts
  
+ A hand object is defined by an array of 5 card objects the following structure:
+ ```json
+{
+    cards: 
+        [
+            {value:5,suit:"C"},
+            {value:5,suit:"D"},
+            {value:8,suit:"S"},
+            {value:9,suit:"S"},
+            {value:7,suit:"S"}
+        ]
+}
+ ```
--- app/pokerLogic.js
+++ app/pokerLogic.js
@@ -1,28 +1,65 @@
-
-function isSequential(pokerHand) {    
+/**
+ * Check to see if the hand has a sequence
+ * @param {Array} pokerHand
+ * @returns boolean
+ */
+function isStraight(pokerHand) {    
+    // console.log(pokerHand.lenght);
     
-
-    pokerHand.sort(function(a,b){return a[0] - b[0]});
+    pokerHand.sort(function(a,b){return a.value - b.value});
 
     for(i=0;i<4;i++){
-        if(pokerHand[i][0]+1 != pokerHand[i+1][0]){
-            return 0;
+        if(pokerHand[i].value+1 != pokerHand[i+1].value){
+            return false;
         }
     }
     
-    return 1;
+    return true;
 }
 
+
+/**
+ * Check to see if the hand is a flush
+ * @param {Array} pokerHand
+ * @returns boolean
+ */
 function isFlush(pokerHand) {
-    for(i=0;i<4;i++){
-        if(pokerHand[i][1] != pokerHand[i+1][1]){
-            return 0;
+    for(i=0;i<5;i++){
+        if(pokerHand[i].suit != pokerHand[i+1].suit){
+            return false;
         }
     }
 
-    return 1;
+    return true;
 }
 
-var hand = [[6,"C"],[5,"D"],[8,"S"],[9,"S"],[7,"S"]];
-console.log("Sequential: " + isSequential(hand));
-console.log("Flush: " + isFlush(hand))
+
+/**
+ * Check to how many of each hand card there is on hand.
+ * @param {Array} pokerHand
+ * @returns Array[Integer,{*}]
+ */
+function manyCheck(pokerHand) {
+    pokerHand.sort(function(a,b){return a[0] - b[0]});
+    var repeating = new Array();
+    var distinctCards = [...new Set(pokerHand.map(x => x.value))];
+    console.log(distinctCards);
+    for(i=0;i<distinctCards.length;i++){
+        console.log("Many ["+distinctCards[i]+"]="+(pokerHand.filter(hand => hand.value === distinctCards[i]).length));
+
+        var repeatingQuantity = (-1 + pokerHand.filter(hand => hand.value === pokerHand[i].value).length);
+        var a = {value:pokerHand[i].value,repetitions:repeatingQuantity};
+        repeating.push(a);
+    }
+    return repeating;
+}
+
+
+
+// A hand is an array of cards
+var hand = {cards: [{value:5,suit:"C"},{value:6,suit:"D"},{value:7,suit:"S"},
+            {value:9,suit:"S"},{value:8,suit:"S"}]}
+console.log("Straight: " + isStraight(hand.cards));
+console.log("Flush: " + isFlush(hand.cards));
+console.log("Deuces: " + manyCheck(hand.cards).length);
+
