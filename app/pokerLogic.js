6f6242ae0acd157a9c966849edc112f967a8ef97
Henrique Nepomuceno <nepo26.hn@gmail.com>
2020-06-29 17:59:14 -0300

    Maybe s

--- app/pokerLogic.js
+++ app/pokerLogic.js
@@ -4,10 +4,7 @@
  * @returns boolean
  */
 function isStraight(pokerHand) {    
-    // console.log(pokerHand.lenght);
-    
     pokerHand.sort(function(a,b){return a.value - b.value});
-
     for(i=0;i<4;i++){
         if(pokerHand[i].value+1 != pokerHand[i+1].value){
             return false;
@@ -24,7 +21,7 @@ function isStraight(pokerHand) {
  * @returns boolean
  */
 function isFlush(pokerHand) {
-    for(i=0;i<5;i++){
+    for(i=0;i<4;i++){
         if(pokerHand[i].suit != pokerHand[i+1].suit){
             return false;
         }
@@ -40,40 +37,133 @@ function isFlush(pokerHand) {
  * @returns repeating
  */
 function manyCheck(pokerHand) {
-    pokerHand.sort(function(a,b){return a[0] - b[0]});
+    // All cards that are repeating
     var repeating = new Array();
+
+    pokerHand.sort(function(a,b){return a[0] - b[0]});
+
+    // All cards without repeated ones
     var distinctCards = [...new Set(pokerHand.map(x => x.value))];
-    console.log(distinctCards);
+
+    // console.log("distintic: "+distinctCards.length);
+    var quantityOfCards ;
     for(i=0;i<distinctCards.length;i++){
-        console.log("Many ["+distinctCards[i]+"]="+(pokerHand.filter(hand => hand.value === distinctCards[i]).length));
-
-        var repeatingQuantity = (-1 + pokerHand.filter(hand => hand.value === pokerHand[i].value).length);
-        repeating.push({value:pokerHand[i].value,repetitions:repeatingQuantity});
+        quantityOfCards = pokerHand.filter(hand => hand.value === distinctCards[i]).length;
+        if (quantityOfCards>=2){
+            var tempRepeat = {value:distinctCards[i],repetitions:quantityOfCards}
+            repeating.push(tempRepeat);
+        }
     }
-
-    repetitions = [...new Set(repeating.map(x => { if (x.repetitions >= 2) return x.repetitions } ))];
-    object = {quantity:repetitions.length,rep:repetitions}
-
+    if(repeating.length!=0){
+        highestRepetition = Math.max.apply(Math,repeating.map(x => x.repetitions ));
+        highestValue = Math.max.apply(Math,repeating.map(x => x.value ));
+    }
+    if(repeating.length==2){
+        // var biggest=repeating.filter(x => x.repetitions==highestRepetition );
+        // var smallest=repeating.filter(x => (x.repetitions!=highestRepetition)||0);
+        console.log("Repeating: "+repeating[0].repetitions);
+        object = {quantity:repeating.length,highestRep:0,rep:{biggest:repeating[0],smallest:repeating[1]}};
+        // if([...new Set(repeating.map(x => x.repetitions))].length >=2){
+        //     object = {quantity:repeating.length,highestRep:0,rep:{biggest:repa,smallest:smallest.pop()}};
+        // }
+        // else{
+        //     object = {quantity:repeating.length,rep:{biggest:biggest.pop(),smallest:smallest.pop()}};
+        // }
+    }
+    else{
+        if(repeating.length!=0){
+            var biggest=repeating.filter(x => x.repetitions==highestRepetition);
+        }
+        else{
+            var biggest = [{value:0,repetitions:0}];
+        }
+        object = {quantity:repeating.length,differentOnes:false,rep:{biggest:biggest.pop(),smallest:{value:0,repetitions:0}}};
+    }
+    
+    // object = {quantity:repeating.length,highestRepetition:highestRepetition,rep:{biggest:,lowest:}repeating}
  
-    console.log(object);
     return object;
 }
 
 /**
  *
- * Evaluete 
+ * Evaluate 
  * @param {*} hand
  * @returns evaluation
  */
 function evaluate(hand){
-
+    const highest = Math.max.apply(Math,hand.map(x => x.value ));
+    const many = manyCheck(hand);
+    console.log("Many: "+ many.rep.smallest.repetitions);
+    if (isStraight(hand)){
+        if(isFlush(hand)){
+            
+            return {name:"SF",highest:highest};
+            // Straight Flush
+        }
+        else{
+            return {name:"S",highest:highest};
+            // Straight
+        }
+    }
+    else{
+        // console.log("Not straight");
+        
+        if(many.quantity!=0){
+            // console.log("Repeat");
+            if(many.rep.biggest.repetitions==4){
+                return {name:"4K",highest:highest};
+                // Four of a kind
+            }
+            else{
+                // console.log("Not 4k");
+                // console.log("Highest Rep: "+many.rep.biggest.repetitions);
+                if(many.rep.biggest.repetitions==3){
+                    if(many.rep.smallest.repetitions!=0){
+                        return {name:"FH",highest:highest};
+                        // Full House
+                    }
+                    else{
+                        return {name:"3K",highest:highest};
+                        // Three of a kind
+                    }
+                }
+                else{
+                    // console.log("Biggest != 3");
+                    if(many.rep.smallest.repetitions!=0){
+                        return {name:"2P",highest:highest}
+                        // Two Pairs
+                    }
+                    else{
+                        return {name:"P",highest:highest}
+                        // Pair
+                    }
+                }
+            }
+        }
+        else{
+            if(isFlush(hand)){
+                return {name:"F",highest:highest}
+                // Flush
+            }
+            else{
+                return {name:"HC",highest:highest}
+                // Highest Card
+            }
+        }
+    }
 }
 
 
 // A hand is an array of 5 card objects
-var hand = {cards: [{value:5,suit:"C"},{value:6,suit:"D"},{value:7,suit:"S"},
-            {value:9,suit:"S"},{value:8,suit:"S"}]}
+var hand = {cards: [{value:6,suit:"S"},
+                    {value:6,suit:"C"},
+                    {value:10,suit:"C"},
+                    {value:10,suit:"D"},
+                    {value:13,suit:"D"}]};
+
 console.log("Straight: " + isStraight(hand.cards));
 console.log("Flush: " + isFlush(hand.cards));
-console.log("Deuces: " + manyCheck(hand.cards));
+console.log("Biggest: " + manyCheck(hand.cards).differentOnes);
+console.log(evaluate(hand.cards));
 
